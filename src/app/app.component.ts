096969611699cc2118b5d7289acc1950f9d5a6d6
Roman15122022 <mallrom1231@gmail.com>
2023-08-05 23:14:44 +0300

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,7 +12,7 @@ import { HomeComponent } from './home/home.component';
         <img class="brand-logo" src="/assets/logo.svg" alt="logo" aria-hidden="true">
       </header>
       <section class="content">
-        <app-home></app-home>
+
       </section>
     </main>
   `,
